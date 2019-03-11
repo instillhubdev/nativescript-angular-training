8446449764738a29cfec1cc32b7006c1de574058
rocky122 <38912058+rocky122@users.noreply.github.com>
2019-03-11 21:59:58 +0530

    navigation in nativescript

---
+++ nativescript-navigation/src/app/challenges/current-challenge/current-challenge.component.ts
@@ -0,0 +1,33 @@
+import { Component, ViewContainerRef } from '@angular/core';
+import { ModalDialogService } from 'nativescript-angular/modal-dialog';
+
+import { DayModalComponent } from '../day-modal/day-modal.component';
+import { UIService } from '~/app/shared/ui.service';
+
+@Component({
+  selector: 'ns-current-challenge',
+  templateUrl: './current-challenge.component.html',
+  styleUrls: ['./current-challenge.component.css'],
+  moduleId: module.id
+})
+export class CurrentChallengeComponent {
+  constructor(
+    private modalDialog: ModalDialogService,
+    private vcRef: ViewContainerRef,
+    private uiService: UIService
+  ) {}
+
+  onChangeStatus() {
+    this.modalDialog
+      .showModal(DayModalComponent, {
+        fullscreen: true,
+        viewContainerRef: this.uiService.getRootVCRef()
+          ? this.uiService.getRootVCRef()
+          : this.vcRef,
+        context: { date: new Date() }
+      })
+      .then((action: string) => {
+        console.log(action);
+      });
+  }
+}
